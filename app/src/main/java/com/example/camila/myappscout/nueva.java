60bf8c8312fc3d930ad1cbe54feb4479374184b5
rayyranax <camila.torresv@usach.cl>
2015-06-03 13:15:21 -0400

    archivos

---
+++ app/src/main/java/com/example/camila/myappscout/nueva.java
@@ -0,0 +1,117 @@
+package com.example.camila.myappscout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class nueva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_nueva);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Cancionero");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        /**MODO TABS EN ACTIONBAR**/
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        /**CREAR TABS**/
+        ActionBar.Tab tab = actionBar.newTab()
+                .setText("Canciones")
+                .setTabListener(new TabsListener(
+                        this, "canciones", Canciones.class));
+        actionBar.addTab(tab);
+        tab = actionBar.newTab()
+                .setText("Danzas")
+                .setTabListener(new TabsListener(
+                        this, "danzas",Danzas.class));
+        actionBar.addTab(tab);
+
+        ListView lista1 = (ListView) findViewById(R.id.listado);
+
+        lista1.setOnItemClickListener(AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)){
+                /// Obtiene el valor de la casilla elegida
+                String itemSeleccionado = adapterView.getItemAtPosition(i).toString();
+
+                // muestra un mensaje
+                Toast.makeText( getApplicationContext(), "Haz hecho click en " + itemSeleccionado, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+}
+    public class TabsListener  implements ActionBar.TabListener {
+
+        private Fragment fragment;
+        private final String tag;
+
+        public TabsListener(Activity activity, String tag, Class cls) {
+            this.tag = tag;
+            fragment = Fragment.instantiate(activity, cls.getName());
+        }
+
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            ft.replace(android.R.id.content, fragment, tag);
+        }
+
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            ft.remove(fragment);
+        }
+
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {}
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_nueva, menu);
+        return true;
+    }
+
+    @Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        //int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+            //return true;
+        //}
+
+        //return super.onOptionsItemSelected(item);
+   // }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+
+    }
+}
